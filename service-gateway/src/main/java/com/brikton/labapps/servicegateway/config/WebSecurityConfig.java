d755b0fccdcccebc261b606ac451d22222a4645e
Milton Bernhardt <mbernhardt@scanntech.com>
2021-08-25 00:46:35 -0300

    front se movió al gateway, falta hacer el login y agregar el token a las request

--- service-gateway/src/main/java/com/brikton/labapps/servicegateway/config/WebSecurityConfig.java
+++ service-gateway/src/main/java/com/brikton/labapps/servicegateway/config/WebSecurityConfig.java
@@ -59,11 +59,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests()
-                    .antMatchers("/**").permitAll()
+                .antMatchers("/page/**").permitAll()
+                .antMatchers("/**").permitAll()
+                .antMatchers("/api/usuario/**").permitAll()
+                .antMatchers("/actuator/**").permitAll()
+                .antMatchers("/login").permitAll()
+                .antMatchers("/register").permitAll()
                 .anyRequest().authenticated();
 
         http.cors().disable();
-
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
@@ -74,11 +78,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         configuration.setAllowedOrigins(ImmutableList.of("*"));
         configuration.setAllowedMethods(ImmutableList.of("HEAD",
                 "GET", "POST", "PUT", "DELETE", "PATCH"));
-        // setAllowCredentials(true) is important, otherwise:
-        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
         configuration.setAllowCredentials(true);
-        // setAllowedHeaders is important! Without it, OPTIONS preflight request
-        // will fail with 403 Invalid CORS request
         configuration.setAllowedHeaders(ImmutableList.of("Authorization", "Cache-Control", "Content-Type"));
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
